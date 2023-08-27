eb31229f0b548df9db8a15c409aa4dbd802fa4b0
akif-122 <74589084+akif-122@users.noreply.github.com>
2023-08-27 18:18:23 +0500

    Add Animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,16 @@ navToggleBtn.addEventListener("click", ()=>{
 mClose.addEventListener("click", ()=>{
     mNav.style.opacity = "0";
     mNav.style.pointerEvents = "none";
-})
+});
+
+
+// AOS ANIMATION
+
+AOS.init({
+    offset: 50, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+  
+  });
